81aa157ae09f800ed10957ab36608830ad7b4ae3
LeonardoARGR <leoaragaogris@outlook.com>
2024-09-27 21:47:53 -0300

    Novos testes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const express = require('express')//para usar o express
+const express = require("express")//para usar o express
 const server = express()//para criar um servidor
+const alunos = require("./src/teste.json")
 
 server.get("/", (req, res) =>{
     return res.json({mensagem:"Hello NODE"})
@@ -7,6 +8,10 @@ server.get("/", (req, res) =>{
 
 server.get("/teste", (req, res) =>{
     return res.json({mensagem:"Teste"})
+})
+
+server.get("/alunos", (req, res) =>{
+    return res.json(alunos)
 })
 
 server.listen(1999, ()=>{

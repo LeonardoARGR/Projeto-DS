7b0dfdc79aa92bc2331a6903ac8e3bf8f528a7fa
LeonardoARGR <leoaragaogris@outlook.com>
2024-09-27 22:02:59 -0300

    Versão inicial do projeto

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express")//para usar o express
 const server = express()//para criar um servidor
 const alunos = require("./src/teste.json")
+const usuarios = require("./src/usuarios.json")
+const habitos = require("./src/habitos.json")
 
 server.get("/", (req, res) =>{
     return res.json({mensagem:"Hello NODE"})
@@ -12,6 +14,14 @@ server.get("/teste", (req, res) =>{
 
 server.get("/alunos", (req, res) =>{
     return res.json(alunos)
+})
+
+server.get("/usuarios", (req, res) =>{
+    return res.json(usuarios)
+})
+
+server.get("/habitos", (req, res) =>{
+    return res.json(habitos)
 })
 
 server.listen(1999, ()=>{

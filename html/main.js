6becd5758b6e0c3c0b8b08898d42545d4745dfe7
LeonardoARGR <leoaragaogris@outlook.com>
2024-11-07 22:30:06 -0300

    Continuando a tela de cadastro

--- html/main.js
+++ html/main.js
@@ -13,6 +13,34 @@ async function carregarUsers(){
     })
 }
 
-async function cadastrarUsuario(){
-    
+async function cadastrarUser(nome, email, senha) {
+    try {    
+        const respostaCadastro = await fetch(`http://localhost:3031/salvar/${nome}/${email}/${senha}`, {
+            method: 'GET',
+        });
+
+        const data = await respostaCadastro.json();
+
+        if (data.resposta) {
+            alert(data.resposta);  // Exibir mensagem de sucesso
+        } else {
+            alert("Erro ao cadastrar o usuário");
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        alert("Erro de requisição");
+    }
 }
+
+document.getElementById('btnCadastrar').addEventListener('click', function() {
+    const nome = document.getElementById('nome_user').value;
+    const email = document.getElementById('email_user').value;
+    const senha = document.getElementById('senha_user').value;
+
+    if(!nome || !email || !senha) {
+        alert("Todos os campos devem ser preenchidos!")
+        return;
+    }
+
+    cadastrarUser(nome, email, senha);
+})
